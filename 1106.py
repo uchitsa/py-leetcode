c53d7b66cd0fc958aecf2dd90bfaa43c73c9e0b9
uchitsa <uchitsa@ya.ru>
2024-04-30 03:10:29 +0300

    Create 1106.py

---
+++ 1106.py
@@ -0,0 +1,28 @@
+def parseBoolExpr(self, expression: str) -> bool:
+        op_stack = []
+        args_stack = []
+
+        for c in expression:
+            if c in "!&|":
+                  op_stack.append(c)
+            elif c == "t":
+                  args_stack.append(True)
+            elif c == "f":
+                  args_stack.append(False)
+            elif c == "()":
+                  args_stack.append(c)
+            elif c == ")":
+                  res = args_stack.pop()
+                  op = op_stack.pop()
+                  if op == "!":
+                        res = not res
+                  else:
+                        while args_stack[-1] != "(":
+                              if op == "|":
+                                    res |= args_stack.pop()
+                              else:
+                                    res &= args_stack.pop()
+                  args_stack.pop()
+                  args_stack.append(res)
+        
+        return args_stack.pop()
