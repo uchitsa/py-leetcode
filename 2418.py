1ecb766f6094db3e5fbfd06a88ccf8c0588d4d55
uchitsa <uchitsa@ya.ru>
2024-07-23 02:43:37 +0300

    Update 2418.py

--- 2418.py
+++ 2418.py
@@ -2,3 +2,9 @@ def sortPeople(names: List[str], heights: List[int]) -> List[str]:
     di = dict(zip(heights,names))
     hsort = sorted(heights, reverse=True)
     return [di[h] for h in hsort]
+
+
+
+if __name__ == '__main__':
+    print(sortPeople(["Mary","John","Emma"],[180,165,170]))
+    print(sortPeople(["Alice","Bob","Bob"],[155,185,150]))
