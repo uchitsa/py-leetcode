ca6f6e101d920a340f2d1339d67feb42b8ac2fc1
uchitsa <uchitsa@ya.ru>
2024-10-18 01:32:29 +0300

    update 670.py

--- 670.py
+++ 670.py
@@ -1,10 +1,14 @@
-class Solution:
-    def maximumSwap(self, num: int) -> int:
-        digs = list(str(num))
-        seelast = {int(d): i for i,d in enumerate(digs)}
-        for i,d in enumerate(digs):
-            for swapd in range(9,int(d),-1):
-                if swapd in seelast and seelast[swapd]>i:
-                    digs[i],digs[seelast[swapd]] = digs[seelast[swapd]],digs[i]
-                    return int(''.join(digs))
-        return num
+def maximumSwap(num: int) -> int:
+    digs = list(str(num))
+    seelast = {int(d): i for i,d in enumerate(digs)}
+    for i,d in enumerate(digs):
+        for swapd in range(9,int(d),-1):
+            if swapd in seelast and seelast[swapd]>i:
+                digs[i],digs[seelast[swapd]] = digs[seelast[swapd]],digs[i]
+                return int(''.join(digs))
+    return num
+
+
+if __name__ == '__main__':
+    print(maximumSwap(2736))
+    print(maximumSwap(9973))
