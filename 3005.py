0126d9779c866fdfe796acfe54d563411e293921
uchitsa <uchitsa@ya.ru>
2025-09-23 01:43:25 +0300

    Update 3005.py

--- 3005.py
+++ 3005.py
@@ -6,11 +6,15 @@ def maxFrequencyElements(nums: List[int]) -> int:
         else:
             freq[n] = 1
     res = 0
-    maxi = max(freq.values())
+    maxi = 0
+    for v in freq.values():
+        maxi = max(maxi, v)
+        
     for v in freq.values():
         if v == maxi:
             res += v
     return res
+        
 
 
 print(maxFrequencyElements([1,2,2,3,1,4]))
