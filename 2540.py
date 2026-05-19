e0b54af18626c9ccc55dfe6c4cdb5910c01780bd
uchitsa <uchitsa@ya.ru>
2026-05-20 02:59:07 +0300

    add 2540
    2540. Minimum Common Value

---
+++ 2540.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        st2 = set(nums2)
+        for n in nums1:
+            if n in st2:
+                return n
+        return -1
